eedfccf723d8a82bb7ad2cda15a1b105ce49bd89
Raj Bhushan Dixit <rajbhushandixit99@gmail.com>
2023-10-13 20:15:10 +0530

    objects in JS.

---
+++ Day-2.js
@@ -0,0 +1,24 @@
+// this JS script is day 2 of 30 days javascript: objects in JS.
+
+// Objects in JavaScript...
+// object reffered as a real world entity in the programming.
+// objects contain properties and methods as well...
+
+// creation of objects using object literals..
+const student={
+    name:"Raj Bhushan Dixit",
+    age:23,
+    course:"BCA",
+    sem:6
+}
+console.log(student)
+
+// creating objetc using object constructor...
+const person=new Object()
+
+// adding properties..
+person.name="Sameer"
+person.age=21
+//accessing properties..
+console.log("Name:",person.name);
+console.log("Age:",person.age);
