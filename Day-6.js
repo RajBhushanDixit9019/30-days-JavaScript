b953d3db961e30dda9a82bcb57774b32bb27dd47
Raj Bhushan Dixit <rajbhushandixit99@gmail.com>
2023-10-17 22:27:25 +0530

    Prototypical Inheritance in JS.

---
+++ Day-6.js
@@ -0,0 +1,37 @@
+// this is day 6 of 30 days of JavaScript: Prototypical Inheritance.
+
+// Prototypical in heritance in JavaScript...
+// it is a way or method in which when we read a property of any object,
+// it it is missing in the object. Then JavaScript automatically take it from object's prototype...
+
+// in this methon a special name '__proto__' is used...
+
+const user={
+    online:"active",
+     work(){
+        console.log(`${this.name} is working`);
+    }
+}
+const guest={
+    name:'Guest'
+}
+const admin={
+    name:'Admin'
+}
+
+// using '__proto__'..
+guest.__proto__ =user
+admin.__proto__=user
+
+// checking the status..
+if(guest.online==='active'){
+    // display's guest is active..
+    console.log(`${guest.name} is ${guest.online}`);
+    guest.work()// display's Guest is working..
+}
+console.log("\n");
+if(admin.online==='active'){
+    // display's admin is active..
+    console.log(`${admin.name} is ${admin.online}`);
+    admin.work()// display's admin is working..
+}
