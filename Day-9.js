dcae87f7c38ec429a7e737cfad1ebe2d072d0a65
Raj Bhushan Dixit <rajbhushandixit99@gmail.com>
2023-10-20 20:30:52 +0530

    JSON in JS.

---
+++ Day-9.js
@@ -0,0 +1,14 @@
+// this day 9 of 30 days JavaScript:JSON ...
+
+// JSON stands for JAvaScript Object Notation...
+
+// it is like object only but, it does not have name to it.
+
+// it is a text-based format to represent the structured data in object sytax
+
+// it is mostly used in server communication 
+// where there is a need to transmit the data to server and to clint back.
+{
+    name: "Raj";
+    age: "23";
+}
