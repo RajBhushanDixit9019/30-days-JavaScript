f83b0fee2f891cf34c484b2bf71028a4cb8be796
Raj Bhushan Dixit <rajbhushandixit99@gmail.com>
2023-10-30 22:29:55 +0530

    If-else and Switch in JS.

---
+++ Day-15.js
@@ -0,0 +1,42 @@
+// this is day 15 of 30 days of JavaScript.
+
+// if-else statement
+let a=10;
+if(a%2===0){
+    console.log(`${a} is Even number`);
+}
+else{
+    console.log(`${a} is Odd number`);
+}
+
+// if-else ladder...
+let n=10;
+if(n===10){
+    console.log("Got 10");
+}
+else if(n===20){
+    console.log("Got 20");
+}
+else if(n===200){
+    console.log("Got 200");
+}
+else{
+    console.log("Got nathing");
+}
+
+// switch statement...
+let d=3;
+switch(d){
+    case 1:
+        console.log("1");
+        break;
+    case 2:
+        console.log("2");
+        break;
+    case 3:
+        console.log("3");
+        break;
+    default:
+        console.log("default");
+        break;
+}
